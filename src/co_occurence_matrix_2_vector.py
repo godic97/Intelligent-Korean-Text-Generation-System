73a7d76b8983678f0034cc77ae90803f213deecf
In-Cheol Shin <incheol.shin97@gmail.com>
2020-07-20 22:28:07 +0900

    change some parameters
    change industry code vector size
    2->15
    15 is more meaningful than 2.

--- src/co_occurence_matrix_2_vector.py
+++ src/co_occurence_matrix_2_vector.py
@@ -8,29 +8,29 @@ dict = np.load("../data/id_industryCode.npy", allow_pickle=True).item()
 U, S, V = randomized_svd(co_occurence, n_components=100, random_state=None)
 # U, S, V = np.linalg.svd(co_occurence)
 
-vector = normalize(U[:, :2])
+vector = normalize(U[:, :15])
 
-a = np.array([vector[dict["D14A14"], 0], vector[dict["D14A14"], 1]])
-b = np.array([vector[dict["D14A13"], 0], vector[dict["D14A13"], 1]])
-print("D14A14: ", a)
-print("D14A13: ", b)
-print(util.cos_similarity(a, b))
+a = vector[dict["D14A14"]]
+b = vector[dict["D14A13"]]
+# print("D14A14: ", a)
+# print("D14A13: ", b)
+print("D14A14 & D14A13: ", util.cos_similarity(a, b))
 
-b = np.array([vector[dict["D05A06"], 0], vector[dict["D05A06"], 1]])
-print("D14A14: ", a)
-print("D05A06: ", b)
-print(util.cos_similarity(a, b))
+b = vector[dict["D05A06"]]
+# print("D14A14: ", a)
+# print("D05A06: ", b)
+print("D14A14 & D05A06: ", util.cos_similarity(a, b))
 
-b = np.array([vector[dict["F05A01"], 0], vector[dict["F05A01"], 1]])
-print("D14A14: ", a)
-print("F05A01: ", b)
-print(util.cos_similarity(a, b))
+b = vector[dict["F05A01"]]
+# print("D14A14: ", a)
+# print("F05A01: ", b)
+print("D14A14 & F05A01: ", util.cos_similarity(a, b))
 
-a = np.array([vector[dict["R13A03"], 0], vector[dict["R13A03"], 1]])
-b = np.array([vector[dict["S01B01"], 0], vector[dict["S01B01"], 1]])
-print("R13A03: ", a)
-print("S01B01: ", b)
-print(util.cos_similarity(a, b))
+a = vector[dict["R13A03"]]
+b = vector[dict["S01B01"]]
+# print("R13A03: ", a)
+# print("S01B01: ", b)
+print("R13A03 & S01B01: ", util.cos_similarity(a, b))
 
 vector_industryCode = {}
 for code in dict:
